2c757a43859c0925ce62a76343db249e9eb5fa57
Sebastian <95218813+Rektoooooo@users.noreply.github.com>
2022-03-04 00:14:57 +0100

    LODE MOREEEEEE FINALY JSEM KOKOT

--- Main.java
+++ Main.java
@@ -7,19 +7,16 @@ public class Main {
 
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\033[0;31m";
-    public static final String ANSI_RED_BRIGHT = "\033[0;91m";
-    public static final String ANSI_RED_BOLD_BRIGHT = "\033[1;91m";
     public static final String ANSI_RED_BACKGROUND = "\033[41m";
-    public static final String ANSI_GREEN = "\033[0;92m";
+    public static final String ANSI_GREEN = "\033[0;32m";
     public static final String ANSI_GREEN_BRIGHT = "\033[0;92m";
     public static final String ANSI_GREEN_BACKGROUND = "\033[42m";
-    public static final String ANSI_BLUE = "\033[0;34m";
-    public static final String ANSI_BLUE_BRIGHT = "\033[0;94m";
     public static final String ANSI_BLUE_BACKGROUND = "\033[44m";
     public static final String ANSI_BLACK = "\033[0;30m";
-    public static final String ANSI_RED_BACKGROUND_BRIGHT = "\033[0;101m";
     public static final String ANSI_YELLOW_BOLD = "\033[1;33m";
+    public static final String ANSI_YELLOW_BRIGHT = "\033[0;93m";
     public static final String ANSI_BLACK_BACKGROUND = "\033[40m";
+    public static final String ANSI_WHITE = "\033[0;37m";
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -31,9 +28,16 @@ public class Main {
         int cislaside = 1;
 
 
-
-
-        int[][] pole = new int[8][8];
+        String[][] pole = new String[][]{
+                {"-", "-", "-", "-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-", "-", "-", "-"},
+                {"-", "-", "-", "-", "-", "-", "-", "-"}
+        };
 
         int[][] polelode = new int[][]{
                 {0, 0, 0, 0, 0, 0, 0, 1},
@@ -48,18 +52,18 @@ public class Main {
 
         while (naboje != 0 && rip > 0) {
 
-            System.out.println(ANSI_BLACK_BACKGROUND +"     A    B    C    D    E    F    G    H  "+ ANSI_RESET);
+            System.out.println(ANSI_BLACK_BACKGROUND + "     A    B    C    D    E    F    G    H  " + ANSI_RESET);
 
-            for (int[] ints : pole) {
-                System.out.print(ANSI_BLACK_BACKGROUND +" " + cislaside + " "+ ANSI_RESET);
+            for (String[] ints : pole) {
+                System.out.print(ANSI_BLACK_BACKGROUND + " " + cislaside + " " + ANSI_RESET);
                 cislaside++;
-                for (int vypis : ints) {
+                for (String vypis : ints) {
 
-                    if (vypis == 0) {
-                        System.out.print(ANSI_BLUE_BRIGHT + ANSI_BLUE_BACKGROUND + "  " + vypis + "  " + ANSI_RESET);
-                    } else if (vypis == 1) {
+                    if (vypis == "-") {
+                        System.out.print(ANSI_BLACK + ANSI_BLUE_BACKGROUND + "  " + vypis + "  " + ANSI_RESET);
+                    } else if (vypis == "O") {
                         System.out.print(ANSI_GREEN_BRIGHT + ANSI_GREEN_BACKGROUND + "  " + vypis + "  " + ANSI_RESET);
-                    } else if (vypis == 2) {
+                    } else if (vypis == "X") {
                         System.out.print(ANSI_RED_BACKGROUND + "  " + vypis + "  " + ANSI_RESET);
                     } else {
                         System.out.print("  " + vypis + "  ");
@@ -69,10 +73,10 @@ public class Main {
             }
             System.out.println();
             if (jednou) {
-                System.out.println("Napoveda : 0 je netrefená pozice, 1 je trefená loď, 2 je trefená voda");
+                System.out.println("Nápověda : " + ANSI_WHITE + "0 je nezasažená pozice," + ANSI_RESET + ANSI_GREEN_BRIGHT + " 1 je zasažená loď," + ANSI_RESET + ANSI_YELLOW_BOLD + " 2 je zasažená voda" + ANSI_RESET);
                 jednou = false;
             }
-            System.out.println("Zadej sloupec A-H a radek 1-8");
+            System.out.println("Zadej sloupec A-H a řádek 1-8");
             int[] vstup = safeSken();
             radek = vstup[1];
 
@@ -80,61 +84,112 @@ public class Main {
             sloupec = vstup[0];
             System.out.println();
             if (polelode[radek][sloupec] == 1) {
-                System.out.println(ANSI_GREEN + "Trefil jsi lod" + ANSI_RESET);
+                System.out.println(ANSI_GREEN_BRIGHT + "Zasáhl jsi loď" + ANSI_RESET);
                 polelode[radek][sloupec] = 3;
-                pole[radek][sloupec] = 1;
+                pole[radek][sloupec] = "O";
                 rip--;
             } else if (polelode[radek][sloupec] == 0) {
-                System.out.println(ANSI_RED + "Nic si netrefil" + ANSI_RESET);
+                System.out.println(ANSI_RED + "Nic si nezasáhl" + ANSI_RESET);
                 polelode[radek][sloupec] = 2;
-                pole[radek][sloupec] = 2;
+                pole[radek][sloupec] = "X";
             } else if (polelode[radek][sloupec] == 2) {
-                System.out.println(ANSI_YELLOW_BOLD + "Sem jsi uz vystrelil je tu voda" + ANSI_RESET);
+                System.out.println(ANSI_YELLOW_BOLD + "Sem jsi už vystřelil je tu voda" + ANSI_RESET);
             } else if (polelode[radek][sloupec] == 3) {
-                System.out.println(ANSI_YELLOW_BOLD + "Sem uz jsi vystrelil je tu lod" + ANSI_RESET);
+                System.out.println(ANSI_YELLOW_BOLD + "Sem jsi už vystřelil je tu loď" + ANSI_RESET);
             }
             naboje--;
-            System.out.println("Mas " + naboje + " Naboju");
-            System.out.println("Zbyva ti sestrelis " + rip + " lodi");
-            System.out.println("Napoveda : 0 je netrefená pozice, 1 je trefená loď, 2 je trefená voda");
+            if (naboje >= 18) {
+                System.out.println(ANSI_GREEN_BRIGHT + "Máš " + naboje + " nábojů" + ANSI_RESET);
+            }
+            else if (naboje >= 10) {
+                System.out.println(ANSI_YELLOW_BRIGHT + "Máš " + naboje + " nábojů" + ANSI_RESET);
+            }
+            else {
+                System.out.println(ANSI_RED + "Máš " + naboje + " nábojů" + ANSI_RESET);
+            }
+            if (rip >= 10) {
+                System.out.println(ANSI_RED + "Zbýva ti sestřelit " + rip + " lodí" + ANSI_RESET);
+            }
+            else if (rip >= 5) {
+                System.out.println(ANSI_YELLOW_BRIGHT + "Zbýva ti sestřelit " + rip + " lodí" + ANSI_RESET);
+            }
+            else {
+                System.out.println(ANSI_GREEN_BRIGHT + "Zbýva ti sestřelit " + rip + " lodí" + ANSI_RESET);
+            }
             cislaside = 1;
-            Thread.sleep(3000);
+            System.out.println("Načítaní Mapy");
+            Thread.sleep(1000);
+            System.out.println(ANSI_RED + "33%" + ANSI_RESET);
+            Thread.sleep(1000);
+            System.out.println(ANSI_YELLOW_BRIGHT + "66%" + ANSI_RESET);
+            Thread.sleep(1000);
+            System.out.println(ANSI_GREEN_BRIGHT + "99%" + ANSI_RESET);
 
+            System.out.println("Nápověda : " + ANSI_WHITE + "0 je nezasažená pozice," + ANSI_RESET + ANSI_GREEN_BRIGHT + " 1 je zasažená loď," + ANSI_RESET + ANSI_YELLOW_BOLD + " 2 je zasažená voda" + ANSI_RESET);
         }
         if (rip == 0 && naboje == 0) {
-            System.out.println(ANSI_GREEN + "KLAAAČ sestrelil jsi posledni lod na posledni naboj" + ANSI_RESET);
+            System.out.println(ANSI_GREEN + "KLAAAČ sestřelil jsi poslední loď na poslední náboj" + ANSI_RESET);
         } else if (rip == 0) {
-            System.out.println(ANSI_GREEN + "Vyhravas sestrelil jsi vsechny lode s " + naboje + " prebytecnejma nabojema" + ANSI_RESET);
+            System.out.println(ANSI_GREEN + "Vyhrávaš sestřelil jsi všechny lodě v zasobníku ti zbýva" + naboje + ANSI_RESET);
         } else if (naboje == 0) {
-            System.out.println(ANSI_RED + "Dosli ti naboje prohravas" + ANSI_RESET);
+            System.out.println(ANSI_RED + "Došli ti náboje zbývalo ti sestřelit " + rip + " lodí prohráváš" + ANSI_RESET);
         }
     }
 
     static int[] safeSken() {
         Scanner sken = new Scanner(System.in);
-        String input = sken.nextLine();
-        if (input.length() == 2) {
+        int[] pls = new int[2];
+        int fix = 0;
 
-            List<String> radky = Arrays.asList(
-                    "1", "2", "3", "4", "5", "6", "7", "8"
-            );
-            List<String> sloupce = Arrays.asList(
-                    "A", "B", "C", "D", "E", "F", "G", "H"
-            );
-            char zradek = input.charAt(1);
-            char zsloupec = input.charAt(0);
-            int iradek = radky.indexOf(String.valueOf(zradek));
-            int isloupec = sloupce.indexOf(String.valueOf(zsloupec));
-            int[] result = new int[]{
-                    isloupec, iradek
-            };
-            return result;
+        char[] sloupce = {
+                '1', '2', '3', '4', '5', '6', '7', '8'
+        };
 
+        char[] radky = {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
+        };
+
+        while (true) {
+
+            String input = sken.nextLine();
+            try {
+                for (char a : radky) {
+                    if (a == (input.charAt(0))) {
+                        fix++;
+                    }
+                }
+                for (char a : sloupce) {
+                    if (a == (input.charAt(1))) {
+                        fix++;
+                    }
+                }
+            }
+            catch (Exception e) {
+                fix = 0;
+                System.out.println(ANSI_RED + "Zadal jsi neplatné souřadnice." + ANSI_RESET + ANSI_YELLOW_BRIGHT + " Zkus to znovu." + ANSI_RESET);
+                continue;
+            }
+
+
+            if (input.length() == 2 && fix == 2) {
+
+                for (int i = 0; i < sloupce.length; i++) {
+                    if (sloupce[i] == input.charAt(1)) {
+                        pls[1] = i;
+                    }
+                }
+                for (int i = 0; i < radky.length; i++) {
+                    if (radky[i] == input.charAt(0)) {
+                        pls[0] = i;
+                    }
+                }
+
+                return pls;
+
+            } else {
+                fix = 0;
+                System.out.println(ANSI_RED + "Zadal jsi neplatné souřadnice." + ANSI_RESET + ANSI_YELLOW_BRIGHT + " Zkus to znovu." + ANSI_RESET);
+            }
         }
-        else {
-            System.out.println("Musis zadat dva znaky nebo jsi neco zadal spatne");
-        }
-            return new int[0];
     }
 }
-
